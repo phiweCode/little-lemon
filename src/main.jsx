93e96fde24b495e5a30dc5923005c1e00652839b
phiweCode <thabo.s.mngoma@gmail.com>
2024-01-16 03:22:42 +0200

    implemented the page components

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createRoutesFromElements,createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
+
+import Header from './components/Header/Header'
+import HeroSection from './components/HeroSection/HeroSection'
+import Highlights from './components/Highlights/Highlights'
+import Testimonials from './components/Testimonials/Testimonials'
+import Footer from './components/Footer/Footer'
+import About from './components/About/About'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path="/hero-section" element={<HeroSection />} />
+      <Route path="/highlight" element={<Highlights />} />
+      <Route path="/testimonial" element={<Testimonials />} />
+      <Route path="/about" element={<About />} />
+    </Route>
+  ))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
